a462791f64587c895b4f98d6cf077ebf2f0aa357
codeWithAmitKumar <work.amitpatra@gmail.com>
2025-01-14 13:30:40 +0530

    setup dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>User Dashboard</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-   
+    <div class="wrapper">
+      <aside id="sidebar">
+            <div class="d-flex justify-content-between p-4"><div class="sidebar-logo">
+                <a href="#">UserDashboard</a>
+            </div>
+            <button class="toggle-btn border-O" type="button"><i id="icon" class='bx bxs-chevrons-right' ></i></box-icon></button>
 
-    <h1>Hey User 🙋‍♂️, This is Your Final Dashboard</h1>
+            </div>
+      </aside>
+      <div class="main"></div>
+    </div>
+    
+   
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<script src="script.js"></script>
+
 </body>
 </html>
