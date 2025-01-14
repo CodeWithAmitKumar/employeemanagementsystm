db8797df31d9c13d88046c0fc13e8c2076aded4a
codeWithAmitKumar <work.amitpatra@gmail.com>
2025-01-14 22:26:16 +0530

    sidebar styling

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     <title>User Dashboard</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -71,7 +73,7 @@
         <span>Instagram</span></a>
 
                 <div class="sidebar-copyright" >
-                    <p >Copyright 2025. All rights reserved.</p>
+                    <p > <abbr title="This project Devloped By Amit Kumar Patra">Copyright 2025. All rights reserved.</abbr></p>
                     </div>
             </div>
             <!-- footer end here -->
